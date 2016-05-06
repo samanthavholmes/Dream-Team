da64e1529bc7fffceb76183b76f2ab9b2a4532d0
Brian Donahue & Hanna Taylor <pair+bpd4tw.hannataylor@devbootcamp.com>
2016-05-06 15:55:01 -0400

    Add sessions helper methods and index route for decks controller

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,1 +1,10 @@
-decks_controller.rb
+get '/decks' do
+  @user = User.find_by(id: session[user_id])
+  @decks = Deck.most_recent
+  if logged_in?
+    erb :'/decks/index'
+  else
+    redirect '/login'
+end
+
+
--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,2 +1,3 @@
 get '/' do
+  redirect '/login'
 end
