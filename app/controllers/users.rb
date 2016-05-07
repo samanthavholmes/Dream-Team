caf1d26992c673fd3282c81736764858d937f85e
Katherine Broner & Samantha Holmes <pair+katherinebroner.samanthavholmes@devbootcamp.com>
2016-05-07 19:48:20 -0400

    Add CSS styling

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -14,9 +14,13 @@ post '/users' do
 end
 
 get '/users/:id' do
-  @played_decks = current_user.played_decks.group(:id)
-  @played_games = current_user.played_games
-  @game_correct_hash = current_user.guesses.where(correct:true).group(:game_id).count
-  @game_incorrect_hash = current_user.guesses.where(correct:false).group(:game_id).count
-  erb :'/users/show'
+  if logged_in?
+    @played_decks = current_user.played_decks.group(:id)
+    @played_games = current_user.played_games
+    @game_correct_hash = current_user.guesses.where(correct:true).group(:game_id).count
+    @game_incorrect_hash = current_user.guesses.where(correct:false).group(:game_id).count
+    erb :'/users/show'
+  else
+    redirect '/'
+  end
 end
