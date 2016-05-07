28ad5a1ebd9e39623b8b751a19d97005f38a6a37
Katherine Broner & Samantha Holmes <pair+katherinebroner.samanthavholmes@devbootcamp.com>
2016-05-06 20:31:25 -0400

    Add has_many guesses to game

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -7,11 +7,12 @@ end
 
 post '/games/:game_id/decks/:deck_id/cards/:id' do
   @deck = Deck.find_by(id: params[:deck_id])
-  @shuffled_deck = @deck.cards.shuffle
-  while @shuffled_deck.length > 0
-    @picked_card = @shuffled_deck.pop
+  @game = Game.find_by(id: params[:game_id])
+  @playable_cards = @deck.cards.shuffle - @game.cards
+  while @playable_cards.length > 0
+    @picked_card = @playable_cards.sample
     # @card = @deck.cards.find_by(id: params[:id])
-    @game = Game.find_by(id: params[:game_id])
+    # @game = Game.find_by(id: params[:game_id])
     if params[:guess]
       @guess = Guess.create(card_id: @picked_card.id, game_id: @game.id)
       if @picked_card.answer.downcase == params[:guess].downcase
--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -12,13 +12,13 @@ get '/decks/:id' do
   @deck = Deck.find_by(id: params[:id])
   @shuffled_deck = @deck.cards.shuffle
   @picked_card = @shuffled_deck.pop
-  # session[:shuffled_deck] = [@shuffled_deck]
   erb :'/decks/show'
 end
 
 post '/decks/:deck_id/cards/:id' do
   @deck = Deck.find(params[:deck_id])
-  @picked_card = @deck.cards.find_by(id: params[:id])
   @game = Game.create(deck_id: @deck.id, user_id: current_user.id)
+  @game.cards << @deck.cards.shuffle
+  @picked_card = @deck.cards.find_by(id: params[:id])
   redirect "/games/#{@game.id}/decks/#{@deck.id}/cards/#{@picked_card.id}"
 end
